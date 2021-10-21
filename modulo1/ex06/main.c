da176abd3715df2838c257582a4186b567dccf12
Pedro_Alves <1201381@isep.ipp.pt>
2021-10-21 16:51:23 +0100

    Modulo 1 - Exercicio 15

--- modulo1/ex06/main.c
+++ modulo1/ex06/main.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include "function.h"
 
+int main(int argc, char **argv)
+{
+    int x = 15;
+    int y = 3;
+    int *ptr = &x;
+    power_ref(ptr, y);
+    printf("%d\n", x);
+    return 0;
+}
