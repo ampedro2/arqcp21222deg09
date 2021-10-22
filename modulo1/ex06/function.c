1d7212227105dd0f9f7e8eacd0af3d92948f7133
Pedro <1201381@isep.ipp.pt>
2021-10-22 20:41:49 +0100

    Modulo 1 - Exercício 6 totalmente testado

--- modulo1/ex06/function.c
+++ modulo1/ex06/function.c
@@ -1,7 +1,11 @@
 void power_ref(int *x, int y){
-    int i;
-    int temp = *x;
-    for (i = 0; i < y-1; i++){
-        *x *= temp;
-    }
+	int i;
+	int temp = *x;
+	if (y == 0) *x = 1;
+	else if (y < 0) *x = 0;
+	else{
+		for (i = 0; i < y-1; i++){
+			*x *= temp;
+		}
+	}
 }
