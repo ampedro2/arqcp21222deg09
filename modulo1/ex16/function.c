225e28449ab6407b0b9c8e7a83deaf0a14285498
Pedro_Fernandes <1201384@isep.ipp.pt>
2021-10-27 22:59:57 +0100

    Modulo 1 - Exercício 16 totalmente testado

---
+++ modulo1/ex16/function.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+
+char* where_exists(char* str1, char* str2){
+	int count = 0;
+	int x=0;
+	int i = 0;
+	int num_car = 0;
+	while(*(str1 + num_car)!='\0'){
+		num_car++;
+	}
+	while(count!=num_car && *(str2 + i)!='\0'){
+		if(count == 0) x = i;	
+		
+		if(*(str2 + i)==*(str1 + count) || *(str2 + i)+32==*(str1 + count) || *(str2 + i)==*(str1 + count)-32){
+			count++;
+		}
+		else{
+			count = 0;
+		}
+		i++;
+	}
+	if(count == num_car && *str1 != 0 && *str2 != 0) return (str2 + x);
+	return NULL;
+}
---
+++ modulo1/ex16/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "function.h"
+
+int main(int argc, char **argv){
+	
+	char* str1 = "ola";
+	char* str2 = "abcola";
+	where_exists(str1, str2);
+}
