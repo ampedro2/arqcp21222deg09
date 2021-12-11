32982159a62f0a8d1228defb28f6028ac9c68c3d
Pedro Alves <1201381@isep.ipp.pt>
2021-12-11 19:32:35 +0000

    Modulo 3 - Exercicio 8 incompleto

---
+++ modulo3/ex08/vec_sum_even.s
@@ -0,0 +1,33 @@
+.section .data
+    .global ptrvec
+	.global num
+	.global even
+.section .text
+.global vec_sum_even
+	vec_sum_even:
+		movl num(%rip), %ecx
+		movq ptrvec(%rip), %rdx
+		movl $0, %esi
+	vec_loop:
+		cmpl $0, %ecx
+		je end
+		
+		movslq (%rdx), %rdi
+		movq %rdi, even(%rip)
+		pushq %rdx
+		pushq %rcx
+		call test_even
+		popq %rcx
+		popq %rdx
+		cmpl $0, %eax
+		je next
+		
+		addl (%rdx), %esi
+		addq $4, %rdx
+		jmp end
+		
+	next:
+		addq $4, %rdx
+		jmp vec_loop
+	end:
+		ret
